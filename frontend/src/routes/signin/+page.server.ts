3848c7b235546a277637f281e7755155a5754f5f
Makuza Mugabo Verite <mugaboverite@gmail.com>
2023-05-27 00:25:56 +0200

    chore: updated

--- frontend/src/routes/signin/+page.server.ts
+++ frontend/src/routes/signin/+page.server.ts
@@ -3,30 +3,33 @@ import { BACKEND_BASE_URL } from "../../utils/constants";
 import type { Actions } from "./$types";
 
 export const actions: Actions = {
-	signin: async ({ request, cookies, setHeaders }) => {
-		const form_data = await request.formData()
-		const request_body = Object.fromEntries(form_data.entries())
+  signin: async ({ request, cookies }) => {
+    const form_data = await request.formData();
+    const request_body = Object.fromEntries(form_data.entries());
 
-		const signin_request = await fetch(`${BACKEND_BASE_URL}/users/signin`, {
-			method: "POST",
-			body: JSON.stringify(request_body),
-			headers: {
-				"Content-Type": "application/json"
-			}
-		})
+    const signin_request = await fetch(`${BACKEND_BASE_URL}/users/signin`, {
+      method: "POST",
+      body: JSON.stringify(request_body),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-		if (!signin_request.ok) {
-			return {
-				signin_error: (await signin_request.json()).message
-			}
-		}
+    if (!signin_request.ok) {
+      return {
+        signin_error: (await signin_request.json()).message,
+      };
+    }
 
-		//console.log(signin_request.headers.get("set-cookie")!)
+    //console.log(signin_request.headers.get("set-cookie")!)
 
-		//TODO: remove session="" string from cookie
-		cookies.set("session", cookies.serialize("session", signin_request.headers.get("set-cookie")!))
+    //TODO: remove session="" string from cookie
+    cookies.set(
+      "session",
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      cookies.serialize("session", signin_request.headers.get("set-cookie")!)
+    );
 
-
-		throw redirect(301, "/")
-	}
+    throw redirect(301, "/");
+  },
 };
